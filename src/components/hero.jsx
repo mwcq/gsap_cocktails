94308142d3e5baf1f9e46db61dc8b4f02a9b17db
liuzhiyuan <908680719@qq.com>
2026-01-28 10:56:21 +0800

    酒杯滚动动画

--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import gsap from 'gsap'
 import { useGSAP } from '@gsap/react'
 import { SplitText } from 'gsap/all'
+import { useMediaQuery } from 'react-responsive'
+import { ScrollTrigger } from 'gsap/all'
+
+gsap.registerPlugin(ScrollTrigger)
 
 const hero = () => {
+    const videoRef = useRef()
+
+    const isMobile = useMediaQuery({ maxWidth: 767 })
+
     useGSAP(() => {
         const heroSplit = new SplitText('.title', { type: 'chars, words' })
         const paragraphSplit = new SplitText('.subtitle', { type: 'lines' })
@@ -38,6 +46,28 @@ const hero = () => {
         })
             .to(".right-leaf", { y: 200 }, 0)
             .to(".left-leaf", { y: -200 }, 0)
+
+        const startValue = isMobile ? " top 50%" : 'center 60%';
+        const endValue = isMobile ? "120% top" : "bottom top";
+
+        // 视频动画
+        const tl = gsap.timeline({
+            scrollTrigger: {
+                trigger: "video",
+                start: startValue,
+                end: endValue,
+                scrub: true,
+                pin: true,
+
+            }
+        })
+
+        videoRef.current.onloadedmetadata = () => {
+            tl.to(videoRef.current,{
+                currentTime:videoRef.current.duration
+            })
+        }
+
     }, [])
     return (
         <>
@@ -64,6 +94,9 @@ const hero = () => {
                 </div>
 
             </section>
+            <div className="viedo absolute inset-0">
+                <video src="/videos/output.mp4" muted playsInline preload='auto' ref={videoRef}></video>
+            </div>
         </>
     )
 }
